6b5fd25d857fbc8d21ffbd5b1e88bb3fbbe6c1a3
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2023-03-01 21:23:03 +0300

    feat(cli)!: update cli model simplified
    remove user, password, host, port, db_name, table_name fields
    add conn_string field

--- data-ingest-rs/src/models/cli_model.rs
+++ data-ingest-rs/src/models/cli_model.rs
@@ -5,29 +5,9 @@ use clap::{builder, ArgGroup, Args};
 #[command(author, version, about, long_about = None)]
 #[command(group(ArgGroup::new("input").required(true).args(["url", "file"])))]
 pub struct Cli {
-    /// The user name to use when connecting to the database.
-    #[arg(short, long, value_parser = builder::NonEmptyStringValueParser::new(), default_value = "root")]
-    user: String,
-
-    /// The password to use when connecting to the database.
-    #[arg(short = 'P', long, value_parser = builder::NonEmptyStringValueParser::new(), default_value = "root")]
-    password: String,
-
-    /// The host to connect to.
-    #[arg(short = 'H', long, default_value_t = 5432)]
-    host: u16,
-
-    /// The port to connect to.
-    #[arg(short, long, default_value_t = 5432)]
-    port: u16,
-
-    /// The name of the database to connect to.
-    #[arg(short = 'n', long = "name", value_parser = builder::NonEmptyStringValueParser::new(), default_value = "postgres")]
-    database_name: String,
-
-    /// The name of the table to ingest data into.
-    #[arg(short = 't', long = "table", value_parser = builder::NonEmptyStringValueParser::new())]
-    table_name: String,
+    /// The connection string to connect database instance
+    #[arg(short, long = "conn", value_parser = builder::NonEmptyStringValueParser::new())]
+    conn_string: String,
 
     /// The URL of the csv or parquet data that will ingest
     #[arg(long, value_parser = builder::NonEmptyStringValueParser::new())]
